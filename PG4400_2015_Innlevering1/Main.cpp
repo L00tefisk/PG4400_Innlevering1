49855b72032b9a935945022ca3f6376bfa737c1f
L00tefisk <Eivind.93@hotmail.com>
2015-03-28 18:44:26 +0100

    Updated paddles.

--- PG4400_2015_Innlevering1/Main.cpp
+++ PG4400_2015_Innlevering1/Main.cpp
@@ -5,49 +5,16 @@
 
 #include "EventHandler.h"
 #include "GameObject.h"
+#include "DrawEngine.h"
+#include "Player.h"
 
 
 int main(int argc, char* argv[])
 {
 	std::shared_ptr<EventHandler> eventHandler = EventHandler::GetInstance();
 	std::shared_ptr<InputManager> inputManager = InputManager::GetInstance();
-
-	if (SDL_Init(SDL_INIT_VIDEO))
-	{
-		std::cerr << "Failed to initialize SDL, details:" << SDL_GetError() << std::endl;
-
-		return EXIT_FAILURE;
-	}
-
-	SDL_Window* window = SDL_CreateWindow(
-		"An awesome SDL window",
-		SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED,
-		1024,
-		576,
-		SDL_WINDOW_SHOWN
-		);
-
-	if (window == NULL)
-	{
-		std::cerr << "Failed to open SDL window, details:" << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return EXIT_FAILURE;
-	}
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-	if (renderer == NULL)
-	{
-		std::cerr << "Failed to create renderer, details:" << SDL_GetError() << std::endl;
-		SDL_Quit();
-		SDL_DestroyWindow(window);
-		return EXIT_FAILURE;
-	}
-
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-	SDL_RenderClear(renderer);
-
-	SDL_Surface* image = IMG_Load("../Resources/sdl2.bmp");
+	Player* player = new Player();
+	/*SDL_Surface* image = IMG_Load("../Resources/sdl2.bmp");
 
 	std::array<SDL_Texture*, 2> textureArray;
 
@@ -93,23 +60,24 @@ int main(int argc, char* argv[])
 		SDL_FreeSurface(sdlBro);
 		return EXIT_FAILURE;
 	}
+	*/
 
 	SDL_Rect bounding_box;
-	bounding_box.h = image->h;
-	bounding_box.w = image->w;
+	bounding_box.h = 1000;//image->h;
+	bounding_box.w = 500;//image->w;
 	bounding_box.x = 0;
 	bounding_box.y = 0;
 
-	SDL_FreeSurface(image);
+	//SDL_FreeSurface(image);
 
 	SDL_Rect broBox;
 
-	broBox.h = sdlBro->h;
-	broBox.w = sdlBro->w;
+	broBox.h = 50;//sdlBro->h;
+	broBox.w = 50;//sdlBro->w;
 	broBox.x = 0;
 	broBox.y = 0;
 
-	SDL_FreeSurface(sdlBro);
+	//SDL_FreeSurface(sdlBro);
 	
 	eventHandler->update();
 
@@ -118,25 +86,19 @@ int main(int argc, char* argv[])
 		if (inputManager->KeyDown(SDL_SCANCODE_Q))
 			eventHandler->exitGame = true;
 
+		player->update();
 		eventHandler->update();
 
 		broBox.x = inputManager->getMouseX() - broBox.w/2;
 		broBox.y = inputManager->getMouseY() - broBox.h/2;
 
-		SDL_RenderCopy(renderer, textureArray[0], NULL, &bounding_box);
-		SDL_RenderCopy(renderer, textureArray[1], NULL, &broBox);
-
-		SDL_RenderPresent(renderer);
+		//drawEngine->Draw(broBox, "../Resources/sdl_bro.bmp");
 	}
 
 	std::cout << "Everything went better than expected!" << std::endl;
 	
 
-	SDL_DestroyTexture(textureArray[0]);
-	SDL_DestroyTexture(textureArray[1]);
+	//drawEngine->~DrawEngine();
 
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
 	return EXIT_SUCCESS;
 }
