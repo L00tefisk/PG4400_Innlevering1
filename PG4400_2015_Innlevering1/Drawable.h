5ea8ddfb8048a03c2089bf53451e25cd5c26186d
klemag <klemag13@nith.no>
2015-04-10 16:57:00 +0200

    Fixed rendering

---
+++ PG4400_2015_Innlevering1/Drawable.h
@@ -0,0 +1,31 @@
+#ifndef DRAWABLE_H
+#define DRAWABLE_H
+
+#include <memory>
+#include <vector>
+#include <SDL.h>
+#include <SDL_image.h>
+
+#include <iostream>
+
+
+class Drawable
+{
+public:
+	Drawable();
+	virtual ~Drawable();
+	virtual void Draw() = 0;
+
+	void loadResource(const std::string &path);
+	static void Init(SDL_Renderer *renderer);
+	
+protected:
+	
+	unsigned short textureID;
+	static SDL_Renderer *renderer;
+	static std::vector<SDL_Texture *> textureList;
+
+private:
+};
+
+#endif
