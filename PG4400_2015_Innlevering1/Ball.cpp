3a0a142411671125b829679cb49aafd1fcb7f5a3
L00tefisk <Eivind.93@hotmail.com>
2015-04-10 18:14:33 +0200

    Creating ball physics

--- PG4400_2015_Innlevering1/Ball.cpp
+++ PG4400_2015_Innlevering1/Ball.cpp
@@ -9,7 +9,25 @@ Ball::~Ball()
 {
 
 }
+
+int hitTest (SDL_Rect rect1, SDL_Rect rect2)
+{
+	if (rect1.x < rect2.x + rect2.w &&
+		rect1.x + rect1.w > rect2.x &&
+		rect1.y < rect2.y + rect2.w &&
+		rect1.h + rect1.y > rect2.y)
+	{
+		//HIT
+	}
+}
+
 void Ball::Update()
 {
-
+	rect.x += xSpeed;
+	rect.y += ySpeed;
+	
+	if (rect.x < 0 || rect.x + rect.w > 800)
+		xSpeed *= -1;
+	if (rect.y < 0 || rect.y + rect.y > 600)
+		ySpeed *= -1;
 }
