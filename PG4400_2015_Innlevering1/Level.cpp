b3fdf84e0fda3306dc92b43954c03a2de82f2e06
klemag <klemag13@nith.no>
2015-04-26 19:47:01 +0200

    stuff.

--- PG4400_2015_Innlevering1/Level.cpp
+++ PG4400_2015_Innlevering1/Level.cpp
@@ -44,55 +44,62 @@ void Level::loadLevel(std::string level)
 	short x, y, w, h;
 	unsigned short textureID;
 	unsigned short type;
-	SDL_Rect rect;
-	std::ifstream inputStream(level.c_str(), std::ifstream::binary | std::ifstream::in);
-	std::vector<std::string> resourcesInUse;
-	if (!inputStream)
-		return;
-
 	unsigned int numResourcesInUse;
 
-	inputStream.read(reinterpret_cast<char*>(&numResourcesInUse), sizeof(unsigned int));
-
-	for (unsigned int i = 0; i < numResourcesInUse; i++)
-	{
-		char buffer[64];
-
-		inputStream.read(buffer, 64);
-		resourcesInUse.push_back(buffer);
-	}
-
-	std::cout << numResourcesInUse << " resources loaded" << std::endl;
-	inputStream.peek();
+	SDL_Rect rect;
+	std::ifstream inputStream;
+	inputStream.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	std::vector<std::string> resourcesInUse;
+	try{
+		inputStream.open(level.c_str(), std::ifstream::binary | std::ifstream::in);
 
 
-	inputStream.read(reinterpret_cast<char*>(&w), sizeof(short));
-	inputStream.read(reinterpret_cast<char*>(&h), sizeof(short));
-	while (!inputStream.eof())
-	{
 
-		inputStream.read(reinterpret_cast<char*>(&x), sizeof(short));
-		inputStream.read(reinterpret_cast<char*>(&y), sizeof(short));
-		inputStream.read(reinterpret_cast<char*>(&type), sizeof(short));
-		inputStream.read(reinterpret_cast<char*>(&textureID), sizeof(unsigned short));
+		inputStream.read(reinterpret_cast<char*>(&numResourcesInUse), sizeof(unsigned int));
+
+		for (unsigned int i = 0; i < numResourcesInUse; i++)
+		{
+			char buffer[64];
+
+			inputStream.read(buffer, 64);
+			resourcesInUse.push_back(buffer);
+		}
+
+		std::cout << numResourcesInUse << " resources loaded" << std::endl;
 		inputStream.peek();
 
-		if (type == 2)
-			indestructibleBricksCount++;
 
-		rect.x = x;
-		rect.y = y;
-		rect.w = w;
-		rect.h = h;
-		Brick b(rect, type);
-		b.loadResource(resourcesInUse[textureID], rect);
-		b.centerX = rect.x + (rect.w / 2);
-		b.centerY = rect.y + (rect.h / 2);
-		
-		AddBrick(b);
+		inputStream.read(reinterpret_cast<char*>(&w), sizeof(short));
+		inputStream.read(reinterpret_cast<char*>(&h), sizeof(short));
+		while (!inputStream.eof())
+		{
+
+			inputStream.read(reinterpret_cast<char*>(&x), sizeof(short));
+			inputStream.read(reinterpret_cast<char*>(&y), sizeof(short));
+			inputStream.read(reinterpret_cast<char*>(&type), sizeof(short));
+			inputStream.read(reinterpret_cast<char*>(&textureID), sizeof(unsigned short));
+			inputStream.peek();
+
+			if (type == 2)
+				indestructibleBricksCount++;
+
+			rect.x = x;
+			rect.y = y;
+			rect.w = w;
+			rect.h = h;
+			Brick b(rect, type);
+			b.loadResource(resourcesInUse[textureID], rect);
+			b.centerX = rect.x + (rect.w / 2);
+			b.centerY = rect.y + (rect.h / 2);
+
+			AddBrick(b);
+		}
+
 	}
-
-	std::cout << map.size() << " objects loaded " << std::endl;
+	catch (std::ifstream::failure e)
+	{
+		std::cout << "failed to open or read file: " << e.what() << std::endl;
+	}
 
 	inputStream.close();
 };
